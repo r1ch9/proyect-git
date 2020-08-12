c338216f535538edde5e57db6e25737433eb1d4c
r1ch9 <r.granizal2019@gmail.com>
2020-08-12 19:06:13 -0300

    login definitivo
    Este login es el final que debemos crear, funcionando bien al menos en este momento, puede que en el futuro cree mas variaciones.

--- js/login.js
+++ js/login.js
@@ -1,26 +1,70 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-function checkInput() {
-    let campoUsuario = document.getElementsById('inputUsr');
-    var textCampoUsuario = (campoUsuario.innerText || campoUsuario.textContet)
-    if (campoUsuario == '') {
-        alertUser;
-    } else {
+var corrPass = false;
+var corrUser = false;
+var imprPass = '';
+var password;
+var user;
+var rememberCheck;
+var elusuariolocal;
+var elusuariosession;
 
-    }
-    alert(textCampoUsuario);
+function checkInput() {
+    rememberCheck = document.getElementById("checkRemember").checked;
+    checkUser();
     checkPassword();
+    if (corrPass && corrUser) {
+        window.location.href = "https://r1ch9.github.io/proyect-git/";
+    }
+    if (rememberCheck) {
+        localStorage.setItem(elusuariolocal, document.getElementById("userName").value);
+    } else {
+        sessionStorage.setItem(elusuariosession, document.getElementById("userName").value);
+    }
 }
+
+function checkPassword() {
+    password = document.getElementById("inputpwd").value;
+    if (password == '') {
+        imprPass = ' <p id = "mensajeUserP" > La contraseña no puede estar vacía.</p>'
+        document.getElementById('imprimiraca').innerHTML = imprPass;
+        document.getElementById('imprimiraca').style.visibility = 'visible';
+        corrPass = false;
+    } else {
+        if (password.length < 5) {
+            imprPass = ' <p id = "mensajeUserP" > La contraseña debe tener mas de 4 digitos.</p>'
+            document.getElementById('imprimiraca').innerHTML = imprPass;
+            document.getElementById('imprimiraca').style.visibility = 'visible';
+            corrPass = false;
+        } else {
+            if (password.length > 12) {
+                imprPass = ' <p id = "mensajeUserP" > La contraseña no puede tener mas de 12 digitos.</p>'
+                document.getElementById('imprimiraca').innerHTML = imprPass;
+                document.getElementById('imprimiraca').style.visibility = 'visible';
+                corrPass = false;
+            } else {
+                document.getElementById('imprimiraca').style.visibility = 'hidden';
+                corrPass = true;
+            }
+        }
+    }
+}
+
+function checkUser() {
+    var user = document.getElementById("userName").value;
+
+    if (user == '') {
+        imprPass = '<p id="mensajeUser">El campo "Usuario" no puede estar vacío.</p>'
+        document.getElementById('mensajeUser').innerHTML = imprPass;
+        document.getElementById('mensajeUser').style.visibility = 'visible';
+        corrUser = false;
+    } else {
+        document.getElementById('mensajeUser').style.visibility = 'hidden';
+        corrUser = true;
+    }
+}
+
 document.addEventListener("DOMContentLoaded", function(e) {
     document.getElementById('mensajeUser').style.visibility = 'hidden';
 });
-
-
-function checkPassword() {
-    alert("password")
-}
-
-function alertUser() {
-    document.getElementById('mensajeUser').style.visibility = 'visible';
-}
