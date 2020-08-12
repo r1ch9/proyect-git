c5bdfb2eea77c8cfbb0c1a2ef6a68cda338fbe56
r1ch9 <r.granizal2019@gmail.com>
2020-08-12 00:48:11 -0300

    advancing with login

--- js/login.js
+++ js/login.js
@@ -1,6 +1,24 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+function checkInput() {
+    let campoUsuario = document.getElementById("inputUsr").value;
+    if (campoUsuario == '') {
+        alertUser;
+    } else {
 
+    }
+    checkPassword();
+}
+document.addEventListener("DOMContentLoaded", function(e) {
+    /*document.getElementById('mensajeUser').style.visibility = 'hidden';*/
 });
+
+
+function checkPassword() {
+    alert("password")
+}
+
+function alertUser() {
+    document.getElementById('mensajeUser').style.visibility = 'visible';
+}
