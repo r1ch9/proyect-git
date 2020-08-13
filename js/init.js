0a5524426c2d36c512d1dc0f538dd5726eaef8a4
r1ch9 <r.granizal2019@gmail.com>
2020-08-13 11:40:17 -0300

    bucle fix
    Habia un bucle en la redireccion al login.html que intento solucionar.

--- js/init.js
+++ js/init.js
@@ -46,9 +46,11 @@ var getJSONData = function(url) {
 //elementos HTML presentes.
 
 document.addEventListener("DOMContentLoaded", function(e) {
-    if (sessionStorage.getItem(elusuariosession) == null) {
-        if (localStorage.getItem(elusuariolocal) == null) {
-            window.location.href = "https://r1ch9.github.io/proyect-git/login.html";
+    if (window.location != "https://r1ch9.github.io/proyect-git/login.html") {
+        if (sessionStorage.getItem(elusuariosession) == null) {
+            if (localStorage.getItem(elusuariolocal) == null) {
+                window.location.href = "https://r1ch9.github.io/proyect-git/login.html";
+            }
         }
     }
 });
