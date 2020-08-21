f2a27a8f7456d08011eec46c441f9e279b70167c
r1ch9 <r.granizal2019@gmail.com>
2020-08-21 04:18:41 -0300

    googleButton v32

--- js/login.js
+++ js/login.js
@@ -77,7 +77,6 @@ function onSignIn() {
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
     console.log("ID Token: " + id_token);
-    checkInput();
 }
 
 function checkUser() {
