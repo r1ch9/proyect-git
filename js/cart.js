5d7151b928ab3e81d4ec0e444143a1c2e8ca94e9
Richard Granizal <r.granizal2019@gmail.com>
2020-09-30 19:18:52 -0300

    progreso entrega 5

--- js/cart.js
+++ js/cart.js
@@ -1,8 +1,73 @@
 const LINK_CARRITO = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
+let deleted = [];
+let j = 0;
+let dataI;
+let elementos = 0;
 
 //Funcion que imprime todos los datos en pantalla
-function imprimirDatos() {
-    alert('se llama a la funcion.');
+function imprimirDatos(array) {
+    dataI = '';
+    object = array.articles;
+
+    for (let i = 0; i < object.length; i++) {
+        dataI += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-2">
+                    <img src="` + object[i].src + `" class="img-thumbnail">
+                </div>
+                <div class="col-5">
+                    <h6 class="text-muted">` + object[i].name + `</h6>
+                </div>
+                <div class="col-3">
+                    <h6 class="text-muted" style="text-align: right">` + object[i].currency + ` ` + object[i].unitCost + `</h6>
+                </div>
+                <div class="col-2">
+                    <input type="number" style="width: 70%" placeholder="` + object[i].count + `">
+                    <span class="close" onclick="deleteElement(` + i + `)">&times;</span>
+                </div>
+            </div>
+        </div>
+        <br/>
+        `;
+    }
+
+    document.getElementById('impresion').innerHTML = dataI;
+}
+
+function deleteElement(element) {
+    deleted[j] = element;
+    dataI = ``;
+
+    if (elementos != 1) {
+        for (let i = 0; i < object.length; i++) {
+            if (i == deleted[j]) {} else {
+                dataI += `
+            <div class="list-group-item list-group-item-action">
+                <div class="row">
+                    <div class="col-2">
+                        <img src="` + object[i].src + `" class="img-thumbnail">
+                    </div>
+                    <div class="col-5">
+                        <h6 class="text-muted">` + object[i].name + `</h6>
+                    </div>
+                    <div class="col-3">
+                        <h6 class="text-muted" style="text-align: right">` + object[i].currency + ` ` + object[i].unitCost + `</h6>
+                    </div>
+                    <div class="col-2">
+                        <input type="number" style="width: 70%" placeholder="` + object[i].count + `">
+                        <span class="close" onclick="deleteElement(` + i + `)">&times;</span>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            `;
+                elementos += 1;
+            }
+        }
+    }
+    j += 1;
+    document.getElementById('impresion').innerHTML = dataI;
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
@@ -11,7 +76,7 @@ function imprimirDatos() {
 document.addEventListener("DOMContentLoaded", function(e) {
     getJSONData(LINK_CARRITO).then(function(resultObj) {
         if (resultObj.status === "ok") {
-            alert('TOdo bien');
+            imprimirDatos(resultObj.data);
         }
     });
 });
