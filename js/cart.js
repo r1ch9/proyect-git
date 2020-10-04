4835a21427c74408fa3a78688e61ce8a32ca827b
Richard Granizal <r.granizal2019@gmail.com>
2020-10-04 02:40:49 -0300

    entrega5

--- js/cart.js
+++ js/cart.js
@@ -5,38 +5,80 @@ let jsonG;
 //Impresion de elementos en pantalla
 function impress(json) {
     let object = json.articles;
-    dataI += `
-        <div class="container border">
-            <div class="row">
-                <div class="col-3">
-                    <img src="` + object[0].src + `">
-                </div>
-                <div class="col-6">
-                    <h5>` + object[0].name + `</h5>
-                </div>
-                <div class="col-3">
-                    <p style="text-muted">` + object[0].currency + ` ` + object[0].unitCost + `</p>
-                </div>
-            </div>
-        </div>
-        `
-    document.getElementById('impresion').innerHTML = dataI;
+    document.getElementById('imagen1').innerHTML = `<img src="` + object[0].src + `" class="img-thumbnail">`;
+    document.getElementById('tittle1').innerHTML = `<h5 class="">` + object[0].name + `</h5>`;
+    document.getElementById('parra1').innerHTML = `<p style="text-align: center">` + object[0].currency + ` ` + object[0].unitCost + `</p>`;
+    document.getElementById('0').value = object[0].count;
+    result(json, 0);
 
+    document.getElementById('imagen2').innerHTML = `<img src="` + object[1].src + `" class="img-thumbnail">`;
+    document.getElementById('tittle2').innerHTML = `<h5 class="">` + object[1].name + `</h5>`;
+    document.getElementById('parra2').innerHTML = `<p style="text-align: center">` + object[1].currency + ` ` + object[1].unitCost + `</p>`;
+    document.getElementById('1').value = object[1].count;
+    result(json, 1);
+
+    totalGral(jsonG);
 }
 
-function result(json) {
-    let costo;
-    let object = json.articles;
-    let valor = document.getElementById('selector').value;
-    if (object[0].currency == 'UYU') {
-        costo = object[0].unitCost / 40;
+function totalGral(json) {
+    let total0 = (document.getElementById('0').value * json.articles[0].unitCost) / 40;
+    let total1 = document.getElementById('1').value * json.articles[1].unitCost;
+    let resultado = total0 + total1;
+    document.getElementById('totalGeneral').innerHTML = `Total: USD ` + resultado;
+}
+
+function windowchange(id) {
+    if (id == 'carrito') {
+        document.getElementById(id).className += " active";
+        document.getElementById('bloqueCarrito').hidden = false;
+        document.getElementById('envio').className = document.getElementById('envio').className.replace(" active", "");
+        document.getElementById('bloqueEnvio').hidden = true;
+        document.getElementById('verificacion').className = document.getElementById('verificacion').className.replace(" active", "");
+        document.getElementById('bloqueVerificacion').hidden = true;
+    } else {
+        if (id == 'envio') {
+            document.getElementById('carrito').className = document.getElementById('carrito').className.replace(" active", "");
+            document.getElementById('bloqueCarrito').hidden = true;
+            document.getElementById(id).className += " active";
+            document.getElementById('bloqueEnvio').hidden = false;
+            document.getElementById('verificacion').className = document.getElementById('verificacion').className.replace(" active", "");
+            document.getElementById('bloqueVerificacion').hidden = true;
+        }
+        if (id == 'verificacion') {
+            document.getElementById('carrito').className = document.getElementById('carrito').className.replace(" active", "");
+            document.getElementById('bloqueCarrito').hidden = true;
+            document.getElementById('envio').className = document.getElementById('envio').className.replace(" active", "");
+            document.getElementById('bloqueEnvio').hidden = true;
+            document.getElementById(id).className += " active";
+            document.getElementById('bloqueVarificacion').hidden = false;
+        }
     }
-    let resultado = valor * costo;
-    document.getElementById('ea').innerHTML = `Resultado = ` + object[1].currency + ` ` + resultado + ``;
 }
 
-function asd() {
-    result(jsonG);
+function result(json, id) {
+    let costo;
+    let resultado;
+    let object = json.articles;
+    let valor1 = document.getElementById(id).value;
+
+    if (object[id].currency == 'UYU') {
+        costo = object[id].unitCost / 40;
+    } else {
+        costo = object[id].unitCost;
+    }
+
+    resultado = valor1 * costo;
+
+    if (id == 0) {
+        document.getElementById('total0').innerHTML = `Total = USD ` + resultado;
+    } else {
+        document.getElementById('total1').innerHTML = `Total = USD ` + resultado;
+    }
+    totalGral(jsonG);
+}
+
+function asd(number) {
+    result(jsonG, number);
 }
 
 
