416a0026848f0a398f13f3e433382b2999976c32
Richard Granizal <r.granizal2019@gmail.com>
2020-10-28 09:26:45 -0300

    agregar animacion para editar usuario, guardar usuario nuevo en objeto, en caso de error no lo hace

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,6 +1,69 @@
+var objectUsuario = { "name": "", "age": null, "eMail": "", "phone": "" };
+
+//Animacion para mostrar campo editar usuario.
+function editObjUser() {
+    $('#editUserData').toggle(500);
+}
+
+//Sobreescribimos la data del JSON
+function changeData() {
+    var nameInput, ageInput, emailInput, phoneInput;
+    nameInput = $('#newName').val();
+    ageInput = $('#newAge').val();
+    emailInput = $('#newEmail').val();
+    phoneInput = $('#newPhone').val();
+
+    //Primero tenemos que verificar que la los input esten vacio.
+    if (nameInput != '' && ageInput != '' && emailInput != '' && phoneInput != '') {
+        //Nos aseguramos de que nigun input tenga el borde rojo de error.
+        $('#newName').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        $('#newAge').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        $('#newEmail').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        $('#newPhone').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+
+        //asignamos estos valores al objeto que define nuestro usuario.
+        objectUsuario.name = nameInput;
+        objectUsuario.age = ageInput;
+        objectUsuario.eMail = emailInput;
+        objectUsuario.phone = phoneInput;
+
+
+    } else {
+        if (nameInput == '') {
+            //En caso de que alguno de los input sea incorrecto le damos el borde rojo.
+            $('#newName').css("border-color", "red");
+        } else {
+            $('#newName').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        }
+
+        if (ageInput == '') {
+            $('#newAge').css("border-color", "red");
+        } else {
+            $('#newAge').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        }
+
+        if (emailInput == '') {
+            $('#newEmail').css("border-color", "red");
+        } else {
+            $('#newEmail').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        }
+
+        if (phoneInput == '') {
+            $('#newPhone').css("border-color", "red");
+        } else {
+            $('#newPhone').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+        }
+    }
+
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e) {
-
+    $('#editUserData').hide();
+    $('#newName').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+    $('#newAge').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+    $('#newEmail').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
+    $('#newPhone').css({ "border-color": "grey", "border-width": "1px", "border-style": "solid", "border-radius": "5px" });
 });
